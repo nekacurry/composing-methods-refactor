c71dd702a56cd019ce31e4030450dfc3932a1916
nekacurry <anneka498@gmail.com>
2022-03-04 19:47:30 -0600

    got rid of commented code

--- get_price.py
+++ get_price.py
@@ -2,17 +2,6 @@
 
 # TODO: Replace temporary variable with extracted method/query
 
-# Code snippet. Not runnable.
-# def get_price():
-#     base_price = quantity * item_price
-#     discount_factor = 0
-#     if base_price > 1000:
-#         discount_factor = 0.95
-#     else:
-#         discount_factor = 0.98
-#     return base_price * discount_factor
-
-
 def get_discount(base_price):
   if base_price > 1000:
     return 0.95
--- introduce_explaining_variable3.py
+++ introduce_explaining_variable3.py
@@ -5,11 +5,11 @@ MEDIUM = 600000
 COOKED_CONSTANT = 0.05
 
 def is_cookeding_criteria_satisfied(time, temperature, pressure, desired_state):
-    cooked_amount = time * temperature * pressure * COOKED_CONSTANT
+  cooked_amount = time * temperature * pressure * COOKED_CONSTANT
 
-    if desired_state == 'well-done' and cooked_amount >= WELL_DONE:
-      return True
-    elif desired_state == 'medium' and cooked_amount >= MEDIUM:
-      return True
-    else:
-      return False
+  if desired_state == 'well-done' and cooked_amount >= WELL_DONE:
+    return True
+  elif desired_state == 'medium' and cooked_amount >= MEDIUM:
+    return True
+  else:
+    return False
